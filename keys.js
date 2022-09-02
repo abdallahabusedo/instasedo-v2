0def129a1f7e17c2833b32f1fbaa219e8bcd4e58
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2022-09-03 01:45:47 +0200

    Delete keys.js

--- keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    MONGODB_URL:"mongodb+srv://posidoon:posidoon1234@cluster0.q5ttjod.mongodb.net/?retryWrites=true&w=majority",
-    JWT_SECRET:"thislkgashfdkgjafopgjaokgjakjfdgjfdg",
-    C_NAME:"posidoon",
-    C_APIKEY:"343672571853948",
-    C_APISEC:"vOud09eSxxKfA1irky5dCQX4jto"
-}
